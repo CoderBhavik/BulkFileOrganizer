c5b3f492d806c2b7737e80192a3e4df4175f1a4f
BhavikSuthar <sutharbhavik54@gmail.com>
2026-06-26 21:51:51 +0530

    File searching and verifying complete

--- organizer.py
+++ organizer.py
@@ -1,5 +1,6 @@
 import pathlib
 import argparse
+import sys
 
 FILE_TYPE_NAME = {
     "Images" : ['.jpeg', '.png', '.jpg', '.gif', '.webp', '.avif', '.raw', '.bmp', '.tif', '.tiff', '.svg', '.psd', '.ico'],
@@ -12,13 +13,29 @@ FILE_TYPE_NAME = {
     "Others" : []
 }
 
+def organize_directory(source_directory : pathlib.Path):
+    
+    print("Organizing Directory = ", source_directory)
+
+    for item in source_directory.iterdir():
+        
+        if item.is_file():
+            file_extension = item.suffix
+            file_name = item.name
+
+            print(f"File found = {file_name}, Extension = {file_extension}")
+        
 
 if "__main__" == __name__:
 
     parser = argparse.ArgumentParser(description="Organizer")
-
     parser.add_argument("source_directory", help="Main directory name you want to organize.")
-
     args = parser.parse_args()
 
-    print(args.source_directory)
+    source_path = pathlib.Path(args.source_directory)
+
+    if not source_path.exists() or not source_path.is_dir():
+       print("Your source path cannot be oganized as it is not an directory or it is a file.")
+       sys.exit(1)
+       
+    organize_directory(source_path)
