9d9f16842d078ba6c137b1d144b68ff94ab12b2b
BhavikSuthar <sutharbhavik54@gmail.com>
2026-06-26 23:12:22 +0530

    Core working completed

--- organizer.py
+++ organizer.py
@@ -1,15 +1,16 @@
 import pathlib
 import argparse
 import sys
+import shutil
 
 FILE_TYPE_NAME = {
     "Images" : ['.jpeg', '.png', '.jpg', '.gif', '.webp', '.avif', '.raw', '.bmp', '.tif', '.tiff', '.svg', '.psd', '.ico'],
     "Videos" : ['.webm', '.mkv', '.flv', '.vob', '.ogv', '.ogg', '.rrc', '.gifv', '.mng', '.mov', '.avi', '.qt', '.wmv', '.yuv', '.rm', '.asf', '.amv', '.mp4', '.m4p', '.m4v', '.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.m4v', '.svi', '.3gp', '.3g2', '.mxf', '.roq', '.nsv', '.flv', '.f4v', '.f4p', '.f4a', '.f4b', '.mod'],
     "Audio" : ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.m4a', '.wma', '.aiff'],
-    "Documents" : ['.pdf', '.txt', '.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx'],
+    "Documents" : ['.pdf', '.txt', '.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx', '.csv'],
     "Archives" : ['.zip', '.rar', '.tar', '.gz'],
-    "Programming_Files" : ['.py', '.htm', '.c', '.cpp', '.js', '.java', '.css'],
-    "Executable_Files" : ['.exe', '.app', '.bin'],
+    "Programming_Files" : ['.py', '.htm', '.c', '.cpp', '.js', '.java', '.css', '.ipynb'],
+    "Executable_Files" : ['.exe', '.app', '.bin', '.msi', '.jar'],
     "Others" : []
 }
 
@@ -23,7 +24,6 @@ def organize_directory(source_directory : pathlib.Path):
             file_extension = item.suffix
             file_name = item.name
 
-            print(f"File found = {file_name}, Extension = {file_extension}")
 
             destination_folder = "Other"
 
@@ -38,13 +38,11 @@ def organize_directory(source_directory : pathlib.Path):
 
             destination_dir.mkdir(parents=True, exist_ok=True)
 
-            
+            destination_file_path = destination_dir / file_name
 
+            shutil.move(item, destination_file_path)
 
-
-
-
-        
+    print("All file moved to its correct position.")
 
 if "__main__" == __name__:
 
