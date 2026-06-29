91134f34cb4e2021a5ac35949472f30709e1ffdb
BhavikSuthar <sutharbhavik54@gmail.com>
2026-06-29 21:57:32 +0530

    config.json created for file_type_name

--- organizer.py
+++ organizer.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import pathlib
 import shutil
@@ -6,18 +7,13 @@ import sys
 
 from tqdm import tqdm
 
-FILE_TYPE_NAME = {
-    "Images" : ['.jpeg', '.png', '.jpg', '.gif', '.webp', '.avif', '.raw', '.bmp', '.tif', '.tiff', '.svg', '.psd', '.ico'],
-    "Videos" : ['.webm', '.mkv', '.flv', '.vob', '.ogv', '.ogg', '.rrc', '.gifv', '.mng', '.mov', '.avi', '.qt', '.wmv', '.yuv', '.rm', '.asf', '.amv', '.mp4', '.m4p', '.m4v', '.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.m4v', '.svi', '.3gp', '.3g2', '.mxf', '.roq', '.nsv', '.flv', '.f4v', '.f4p', '.f4a', '.f4b', '.mod'],
-    "Audio" : ['.mp3', '.wav', '.flac', '.aac', '.ogg', '.m4a', '.wma', '.aiff'],
-    "Documents" : ['.pdf', '.txt', '.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx', '.csv'],
-    "Archives" : ['.zip', '.rar', '.tar', '.gz'],
-    "Programming_Files" : ['.py', '.htm', '.c', '.cpp', '.js', '.java', '.css', '.ipynb'],
-    "Executable_Files" : ['.exe', '.app', '.bin', '.msi', '.jar'],
-    "Others" : []
-}
+def load_config(config_path: pathlib.Path):
+    with open(config_path, 'r') as config_file:
+        config_data = json.load(config_file)
+    
+    return config_data
 
-def organize_directory(source_directory : pathlib.Path, dry_run : bool):
+def organize_directory(source_directory : pathlib.Path, dry_run : bool, file_type_map: dict):
     
     logging.info(f"Organizing Directory = {source_directory}")
 
@@ -35,7 +31,7 @@ def organize_directory(source_directory : pathlib.Path, dry_run : bool):
 
         destination_folder = "Other"
 
-        for category, extension in FILE_TYPE_NAME.items():
+        for category, extension in file_type_name.items():
             if file_extension in extension:
                 destination_folder = category
                 break
@@ -92,10 +88,14 @@ if "__main__" == __name__:
         ]
     )
 
+    config_path_name = pathlib.Path(__file__).parent / "config.json"
+
+    file_type_name = load_config(config_path_name)
+
     source_path = pathlib.Path(args.source_directory)
 
     if not source_path.exists() or not source_path.is_dir():
        print("Your source path cannot be oganized as it is not an directory or it is a file.")
        sys.exit(1)
        
-    organize_directory(source_path, args.dry_run)
+    organize_directory(source_path, args.dry_run, file_type_name)
